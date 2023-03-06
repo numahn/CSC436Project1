a1961edc46b68f152679ab653fb8cf933c2b3349
Noman <80441451+nali556@users.noreply.github.com>
2023-03-06 13:59:51 -0500

    suceessful call to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { React, useState } from 'react'
+import axios from 'axios'
 import Navbar from './components/Navbar'
 function App() {
-
+  axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then((response) => {
+      const data = response.data
+      const bpi = data.bpi
+      console.log(bpi)
+    }).catch((err) => {
+      console.log(err)
+    })
   return (
     <>
       <Navbar/>
