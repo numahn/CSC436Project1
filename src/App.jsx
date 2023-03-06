c7408377c17b834229d08407514197f8db222045
Noman <80441451+nali556@users.noreply.github.com>
2023-03-06 13:30:04 -0500

    created navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { React, useState } from 'react'
-import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
 
   return (
     <>
-      Hello world!
+      <Navbar/>
     </>
   )
 }
